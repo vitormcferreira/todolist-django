6f05e5312cc4bfe2aadba507544f0a7f3b92148f
Vitor <>
2023-01-02 22:34:25 -0300

    remover default do campo user

--- todolist/models.py
+++ todolist/models.py
@@ -1,8 +1,9 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 
 
 class ToDoItem(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, blank=True, null=True)
     title = models.CharField(max_length=255)
     text = models.TextField(blank=True)
